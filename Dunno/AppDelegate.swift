377eb7dffe4372cca341468dcd897ee6f5c7a079
Pedro Nascimento <pnascimento@gmail.com>
2015-01-13 17:27:51 -0200

    Using icons, yey

--- Dunno/AppDelegate.swift
+++ Dunno/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        var fontNames : Array = UIFont.familyNames()
+        for font in fontNames {
+            println(font)
+            println(UIFont.fontNamesForFamilyName(font as String))
+        }
+        
         // Override point for customization after application launch.
         return true
     }
