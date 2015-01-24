617065b5c1429a7b64c4b3d5190f0b78c7523ca8
Pedro Nascimento <pnascimento@gmail.com>
2015-01-24 09:59:16 -0200

    Moving extensions into their own group, some minor graphic fixes.

---
+++ Dunno/NSDateExtension.swift
@@ -0,0 +1,21 @@
+//
+//  NSDateExtension.swift
+//  Dunno
+//
+//  Created by Pedro Nascimento on 1/24/15.
+//  Copyright (c) 2015 Pedro Nascimento. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate
+{
+    convenience
+    init(dateString:String) {
+        let dateStringFormatter = NSDateFormatter()
+        dateStringFormatter.dateFormat = "yyyy-MM-dd"
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let d = dateStringFormatter.dateFromString(dateString)
+        self.init(timeInterval:0, sinceDate:d!)
+    }
+}
