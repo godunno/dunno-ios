acbafecc1f3aabb3d045aece88bd21c63e1ecea9
Pedro Nascimento <pnascimento@gmail.com>
2015-02-08 21:42:06 -0200

    Adding some configs for view in Storyboard.

---
+++ Dunno/UIViewExtension.swift
@@ -0,0 +1,71 @@
+//
+//  UIViewExtension.swift
+//  Dunno
+//
+//  Created by Pedro Nascimento on 2/8/15.
+//  Copyright (c) 2015 Pedro Nascimento. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(CGColor: layer.borderColor)
+        }
+        set {
+            layer.borderColor = newValue?.CGColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(CGColor: layer.borderColor)
+        }
+        set {
+            layer.shadowColor = newValue?.CGColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+}
